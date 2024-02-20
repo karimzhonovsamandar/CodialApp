4b23cd6f0ea1f7b964a822bf8df3b4ccfa9d184d
user <dovidovalisher@gmail.com>
2024-02-20 14:53:23 +0500

    lala

---
+++ app/src/main/java/com/sammy/codialapp/Adapter/MyCodial_Adapter.kt
@@ -0,0 +1,30 @@
+package com.sammy.codialapp.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+/*
+class MyCodial_Adapter (var list: ArrayList<user>): RecyclerView.Adapter<Adabter.Vh>() {
+
+    inner class Vh(val itemRvBinding: ) : RecyclerView.ViewHolder(itemRvBinding.root) {
+
+        fun onBind(user: user) {
+            itemRvBinding.tvName.text = user.name
+            itemRvBinding.tvView.text = user.number
+            itemRvBinding.tvDate.text = user.date
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Vh {
+        return Vh(ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: Vh, position: Int) {
+        holder.onBind(list[position])
+    }
+}*/
